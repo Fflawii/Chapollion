34086a977013457b4a2b97af00807fff8850af5f
Flavie Delotre <asuna7545@gmail.com>
2024-05-22 17:33:27 +0200

    Auto stash before merge of "Dev" and "origin/Dev"

--- Assets/Chapollion/ScriptableObjects/Data/Effets.cs
+++ Assets/Chapollion/ScriptableObjects/Data/Effets.cs
@@ -7,42 +7,4 @@ namespace  Chapollion.ScriptableObjects.Data
         public abstract void Apply(Chat aChat);
     }
    
-   [CreateAssetMenu()]
-    public class CharacteristiqueModifierEffect : Effet
-    {
-
-        [SerializeField] private Charactéristique CharactéristiqueToAffect;
-        [SerializeField][Range(-2,2)] private int valueToApply;
-        
-        public override void Apply(Chat aChat)
-        {
-            switch (CharactéristiqueToAffect)
-            {
-                case Charactéristique.Griffe:
-                    aChat.Griffre += valueToApply;
-                    break;
-                case Charactéristique.Poil:
-                    aChat.Poil += valueToApply;
-                    break;
-                case Charactéristique.Oeil:
-                    aChat.Oeil += valueToApply;
-                    break;
-                case Charactéristique.Queue:
-                    aChat.Queue += valueToApply;
-                    break;
-                case Charactéristique.Caresse:
-                    aChat.Caresse += valueToApply;
-                    break;
-                case Charactéristique.Ronronnement:
-                    aChat.Ronronnement += valueToApply;
-                    break;
-                case Charactéristique.Coussinet:
-                    aChat.Coussinet += valueToApply;
-                    break;
-                case Charactéristique.Vibrisse:
-                    aChat.Vibrisse += valueToApply;
-                    break;
-            }
-        }
-    }
 }
